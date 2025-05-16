793c3106323be731d9dd474e6e6c173be90a6c0b
Maria1600 <mariagar.2023.alumn@cescristorey.com>
2025-05-16 16:34:35 +0200

    Hecho mapeado de publicacion

---
+++ commArt/Modelos/Publicacion.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Publicacion(db.Model):
+    #Definimos explicitamente el nombre de la tabla
+    __tablename__ = 'publicaciones'
+
+    #Mapeamos los atributos
+    id_publicacion = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    descripcion_publicacion = db.Column(db.String)
+    dibujo = db.Column(db.String, nullable=False)
+    fecha_publicacion = db.Column(db.Date)
+    num_likes = db.Column(db.Integer)
+
+    #Relaciones
+    #Falta artista asociado
+    #id_artista_publicacion = db.Column(db.Integer, db.ForeignKey('artistas.id_artista'))
+
+    #Funcion para debugg futuro para imprimir datos en consola
+    def __repr__(self):
+        return f"<Publicacion {self.id_publicacion} - {self.descripcion_publicacion} - {self.fecha_publicacion} - {self.num_likes} >"
