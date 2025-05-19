0a953dfd1e2bfe872af31f5c86ed010dd5b193c1
Maria1600 <mariagar.2023.alumn@cescristorey.com>
2025-05-19 16:15:52 +0200

    Terminado al completo el mapeado con relaciones de Comision

--- commArt/Modelos/Comision.py
+++ commArt/Modelos/Comision.py
@@ -12,12 +12,15 @@ class Comision(db.Model):
     estado = db.Column(db.String, nullable=False)
     fecha_creacion = db.Column(db.Date, nullable=False)
     tipo = db.Column(db.String, nullable=False)
+    id_cliente_com = db.Column(db.Integer, db.ForeignKey('usuarios.id_usuario'), nullable=False)
+    id_artista_com = db.Column(db.Integer, db.ForeignKey('artistas.id_artista'))
 
     #Relaciones
-    #Faltan user cliente y user artista + lista de mensajes
-    #id_cliente_com = db.Column(db.Integer, db.ForeignKey('usuarios.id_usuario'))
-    #id_artista_com = db.Column(db.Integer, db.ForeignKey('artistas.id_artista'))
+    #Faltan lista de mensajes
+    artista = db.relationship("Artista", back_populates="comisiones_realizadas")
+    cliente = db.relationship("Usuario", back_populates="comisiones_solicitadas")
+    mensajes =  db.relationship("Mensaje", back_populates="comision_asociada")
 
-    #Funcion para debugg futuro para imprimir datos en consola
+#Funcion para debugg futuro para imprimir datos en consola
     def __repr__(self):
         return f"<Comision {self.id_com} - {self.estado} - {self.tipo} - {self.fecha_creacion} - {self.descripcion_com} >"
