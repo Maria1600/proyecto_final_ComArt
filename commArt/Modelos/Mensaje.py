5fdcbe6ba9096f0842ad16ac606e0bc0fd59b8c0
Maria1600 <mariagar.2023.alumn@cescristorey.com>
2025-05-19 16:16:03 +0200

    Terminado al completo el mapeado con relaciones de Mensaje

--- commArt/Modelos/Mensaje.py
+++ commArt/Modelos/Mensaje.py
@@ -13,11 +13,12 @@ class Mensaje(db.Model):
     #Aunque este obsoleto el utcnow no nos interesa guardar nada mas la hora y le fecha
     #Si se requiere a futuro zona horaria tambien datetime.now(UTC)
     fecha_creacion_mensaje = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    id_user_creador = db.Column(db.Integer, db.ForeignKey('usuarios.id_usuario'), nullable=False)
+    id_com_asociada = db.Column(db.Integer, db.ForeignKey('artistas.id_artista'), nullable=False)
 
     #Relaciones
-    #Faltan user cliente y user artista
-    #id_user_creador = db.Column(db.Integer, db.ForeignKey('usuarios.id_usuario'))
-    #id_com_asociada = db.Column(db.Integer, db.ForeignKey('artistas.id_artista'))
+    creador = db.relationship("Usuario", back_populates="mensajes")
+    comision_asociada = db.relationship("Comision", back_populates="mensajes")
 
     #Funcion para debugg futuro para imprimir datos en consola
     def __repr__(self):
